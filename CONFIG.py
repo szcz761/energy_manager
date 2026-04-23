ee02519d384e7711b6f95d8010b451985ebf3cbc
szcz761 <szcz761@gmail.com>
2026-04-23 07:26:59 +0200

    update config

--- CONFIG.py
+++ CONFIG.py
@@ -2,8 +2,8 @@ PRICE_POWER_I_BUY = 1.38  # current electricity buy price
 TRESHOLD_PRICE_POWER_GAS = 0.39  #  calculated threshold price for buying electricity vs using gas for heating
 UNDER_TRHESHOLD_FOR_LONGER_SELL = 0.2  # if it's very sunny, we can be more aggressive in selling to the grid, so we lower the threshold by this amount
 
-SUNNY_CLOUD_THRESHOLD: int = 70  # % cloud cover
-VERY_SUNNY_CLOUD_THRESHOLD: int = 20  # % cloud cover
+SUNNY_CLOUD_THRESHOLD: int = 85  # % cloud cover
+VERY_SUNNY_CLOUD_THRESHOLD: int = 35  # % cloud cover
 LAT = 51.6397598763277
 LON = 17.78994335885742
 TIMEZONE = "Europe/Warsaw"
